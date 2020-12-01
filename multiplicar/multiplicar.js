adbc7f83d056924afec9731bdcbda48cf75c38b9
Edwin Aponte <aponte847@gmail.com>
2020-12-01 16:14:51 -0300

    Commit Inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,51 @@
+// requireds
+
+const fs = require('fs');
+const colors = require('colors');
+
+
+
+let listarTabla = (base , limite = 10) => {
+
+    console.log( '======================================'.green );
+    console.log( `==============tabla de ${base}==============`.green );
+    console.log( '======================================'.green );
+
+    for (let i = 1 ; i <= limite ; i++) {
+        console.log( `${base} * ${i} = ${base * i}` );
+    }
+
+}
+
+
+let crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if ( !Number(base) ) {
+            reject(`El valor introducido ${base}, no es un numero `);
+            return;
+        }
+
+        let contenido = '';
+
+        for (let i = 1; i <= limite; i++) {
+
+            contenido += (`${base} * ${i} = ${base * i}\n`);
+        }
+
+        // const data = new Uint8Array(Buffer.from(contenido));
+        fs.writeFile(`tablas/tabla - ${base} - al - ${limite}.txt`, contenido, (err) => {
+
+            if (err) reject(err);
+            else
+                resolve (`tabla-${base} - al - ${limite}.txt`)
+        });
+
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
